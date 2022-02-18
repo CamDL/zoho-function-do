17774ba88375cab9ec51348b0008d4b65f581b94
Cameron Letch <4852986+CamDL@users.noreply.github.com>
2022-02-18 15:58:22 -0500

    Refactored for DO Functions

--- server/packages/default/rug/__main__.py
+++ server/packages/default/rug/__main__.py
@@ -1,19 +1,21 @@
 import os
-import requests
-from oauthlib.oauth2 import BackendApplicationClient
+from dotenv import load_dotenv
 from requests_oauthlib import OAuth2Session
-import json
+
+load_dotenv()
 
 envars = os.environ
 client_id = envars['CLIENT_ID']
 client_secret = envars['CLIENT_SECRET']
 
+"""
 #Zoho API Settings
-client = BackendApplicationClient(client_id=client_id)
-oauth = OAuth2Session(client=client)
+#client = BackendApplicationClient(client_id=client_id)
+#oauth = OAuth2Session(client=client)
 
-token = oauth.fetch_token(token_url='https://accounts.zoho.com/oauth/v2/token?grant_type=authorization_code', client_id=client_id,
-    client_secret=client_secret)
+#token = oauth.fetch_token(token_url='https://accounts.zoho.com/oauth/v2/token?grant_type=authorization_code', client_id=client_id,
+#client_secret=client_secret)
+"""
 
 def token_saver(token):
     envars['OAUTH_TOKEN'] = token
@@ -33,13 +35,19 @@ extra = {
 client = OAuth2Session(client_id, token=token, auto_refresh_url=refresh_url,
     auto_refresh_kwargs=extra, token_updater=token_saver)
 
+client.refresh_token(refresh_url)
+
+"""
 #rugs = client.get('https://creator.zoho.com/api/v2/troylusk/cleaning-process/report/Rug_Information_Report').json()
 
 #rug = client.get(f"https://creator.zoho.com/api/v2/troylusk/cleaning-process/report/Rug_Information_Report/{rugs['data'][1]['ID']}").json()
 
 #jsonval  = json.dumps(rug['data'])
 
+#main({"ID":"3183625000003900011"})
+"""
+
 def main(args):
-      ID = args.get("ID")
-      rug = client.get(f"https://creator.zoho.com/api/v2/troylusk/cleaning-process/report/Rug_Information_Report/{ID}").json()
-      return rug
+    ID = args.get("ID")
+    rug = client.get(f"https://creator.zoho.com/api/v2/troylusk/cleaning-process/report/Rug_Information_Report/{ID}").json()
+    return {"body":rug}
