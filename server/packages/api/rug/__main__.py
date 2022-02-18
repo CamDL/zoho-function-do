0e96ca796014050fc4812974e7af7e3c90534418
Cameron Letch <4852986+CamDL@users.noreply.github.com>
2022-02-18 01:57:36 -0500

    Moved some code back to main

--- server/packages/api/rug/__main__.py
+++ server/packages/api/rug/__main__.py
@@ -10,26 +10,25 @@ import json
 
 #jsonval  = json.dumps(rug['data'])
 
-envars = os.environ
-client_id = envars['CLIENT_ID']
-client_secret = envars['CLIENT_SECRET']
-
-token = {
-    'access_token': envars['ACCESS_TOKEN'],
-    'refresh_token': envars['REFRESH_TOKEN'],
-    'token_type': 'Bearer',
-    'expires_in': envars['EXPIRES_IN'],
-}
-refresh_url = 'https://accounts.zoho.com/oauth/v2/token'
-extra = {
-    'client_id': client_id,
-    'client_secret': client_secret,
-}
-
 def token_saver(token):
     envars['OAUTH_TOKEN'] = token
 
 def main(args):
+    envars = os.environ
+    client_id = envars['CLIENT_ID']
+    client_secret = envars['CLIENT_SECRET']
+
+    token = {
+        'access_token': envars['ACCESS_TOKEN'],
+        'refresh_token': envars['REFRESH_TOKEN'],
+        'token_type': 'Bearer',
+        'expires_in': envars['EXPIRES_IN'],
+    }
+    refresh_url = 'https://accounts.zoho.com/oauth/v2/token'
+    extra = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+    }
     # After updating the token you will most likely want to save it.
     #client = OAuth2Session(client_id, token=token, auto_refresh_url=refresh_url,
     #    auto_refresh_kwargs=extra)
