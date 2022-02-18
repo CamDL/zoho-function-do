c7ad92522c1d99898a8955cf60c2af33618c5cbf
Cameron Letch <4852986+CamDL@users.noreply.github.com>
2022-02-18 02:26:51 -0500

    Changed to news api request

--- server/packages/api/rug/__main__.py
+++ server/packages/api/rug/__main__.py
@@ -1,40 +1,7 @@
-import os
 import requests
-from oauthlib.oauth2 import BackendApplicationClient
-from requests_oauthlib import OAuth2Session
-import json
-
-#rugs = client.get('https://creator.zoho.com/api/v2/troylusk/cleaning-process/report/Rug_Information_Report').json()
-
-#rug = client.get(f"https://creator.zoho.com/api/v2/troylusk/cleaning-process/report/Rug_Information_Report/{rugs['data'][1]['ID']}").json()
-
-#jsonval  = json.dumps(rug['data'])
-
-def token_saver(token):
-    os.environ['OAUTH_TOKEN'] = token
 
 def main(args):
-    try:
-        client_id = os.environ.get('CLIENT_ID')
-        client_secret = os.environ.get('CLIENT_SECRET')
-    except:
-        print('Could not retrieve env vars')
-    """
-    token = {
-        'access_token': envars['ACCESS_TOKEN'],
-        'refresh_token': envars['REFRESH_TOKEN'],
-        'token_type': 'Bearer',
-        'expires_in': envars['EXPIRES_IN'],
-    }
-    refresh_url = 'https://accounts.zoho.com/oauth/v2/token'
-    extra = {
-        'client_id': client_id,
-        'client_secret': client_secret,
-    }
-    """
-    # After updating the token you will most likely want to save it.
-    #client = OAuth2Session(client_id, token=token, auto_refresh_url=refresh_url,
-    #    auto_refresh_kwargs=extra)
-    ID = args.get("ID")
-    #rug = client.get(f"https://creator.zoho.com/api/v2/troylusk/cleaning-process/report/Rug_Information_Report/{ID}").json()
-    return str(ID)
+    cat = args.get("category","all")
+    payload = {'category' : cat}
+    response = requests.get('https://inshortsapi.vercel.app/news', params=payload)
+    return response.json()
