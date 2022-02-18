974666c69f2304f8ebfd2b35f529d446750de662
Cameron Letch <4852986+CamDL@users.noreply.github.com>
2022-02-18 01:45:13 -0500

    Added build, changed main

--- server/packages/api/rug/rug.py
+++ server/packages/api/rug/rug.py
@@ -10,35 +10,29 @@ import json
 
 #jsonval  = json.dumps(rug['data'])
 
+envars = os.environ
+client_id = envars['CLIENT_ID']
+client_secret = envars['CLIENT_SECRET']
+
+token = {
+    'access_token': envars['ACCESS_TOKEN'],
+    'refresh_token': envars['REFRESH_TOKEN'],
+    'token_type': 'Bearer',
+    'expires_in': envars['EXPIRES_IN'],
+}
+refresh_url = 'https://accounts.zoho.com/oauth/v2/token'
+extra = {
+    'client_id': client_id,
+    'client_secret': client_secret,
+}
+
+def token_saver(token):
+    envars['OAUTH_TOKEN'] = token
+
 def main(args):
-    envars = os.environ
-    client_id = envars['CLIENT_ID']
-    client_secret = envars['CLIENT_SECRET']
-
-    #Zoho API Settings
-    client = BackendApplicationClient(client_id=client_id)
-    oauth = OAuth2Session(client=client)
-
-    token = oauth.fetch_token(token_url='https://accounts.zoho.com/oauth/v2/token?grant_type=authorization_code', client_id=client_id,
-        client_secret=client_secret)
-
-    def token_saver(token):
-        envars['OAUTH_TOKEN'] = token
-
-    token = {
-        'access_token': envars['ACCESS_TOKEN'],
-        'refresh_token': envars['REFRESH_TOKEN'],
-        'token_type': 'Bearer',
-        'expires_in': envars['EXPIRES_IN'],
-    }
-    refresh_url = 'https://accounts.zoho.com/oauth/v2/token'
-    extra = {
-        'client_id': client_id,
-        'client_secret': client_secret,
-    }
     # After updating the token you will most likely want to save it.
     client = OAuth2Session(client_id, token=token, auto_refresh_url=refresh_url,
-        auto_refresh_kwargs=extra, token_updater=token_saver)
+        auto_refresh_kwargs=extra)
     ID = args.get("ID")
     rug = client.get(f"https://creator.zoho.com/api/v2/troylusk/cleaning-process/report/Rug_Information_Report/{ID}").json()
     return rug
