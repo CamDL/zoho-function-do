c6941588bed8a2d059658e97e263cc9da548c10f
Cameron Letch <4852986+CamDL@users.noreply.github.com>
2022-02-18 02:30:38 -0500

    hello.py

--- server/packages/api/rug/__main__.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def main(args):
-    cat = args.get("category","all")
-    payload = {'category' : cat}
-    response = requests.get('https://inshortsapi.vercel.app/news', params=payload)
-    return response.json()
