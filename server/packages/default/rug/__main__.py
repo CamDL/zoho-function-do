9bb675d73fb62d76d5cd2cfd9ccfc190cd248394
Cameron Letch <4852986+CamDL@users.noreply.github.com>
2022-02-18 16:16:04 -0500

    Removed comment blocks from main

--- server/packages/default/rug/__main__.py
+++ server/packages/default/rug/__main__.py
@@ -2,32 +2,12 @@ import os
 from dotenv import load_dotenv
 from requests_oauthlib import OAuth2Session
 
-"""
-#rugs = client.get('https://creator.zoho.com/api/v2/troylusk/cleaning-process/report/Rug_Information_Report').json()
-
-#rug = client.get(f"https://creator.zoho.com/api/v2/troylusk/cleaning-process/report/Rug_Information_Report/{rugs['data'][1]['ID']}").json()
-
-#jsonval  = json.dumps(rug['data'])
-
-#main({"ID":"3183625000003900011"})
-"""
-
 def main(args):
+    #main({"ID":"3183625000003900011"})
     ID = args.get("ID")
-    load_dotenv()
-
     envars = os.environ
     client_id = envars['CLIENT_ID']
     client_secret = envars['CLIENT_SECRET']
-
-    """
-    #Zoho API Settings
-    #client = BackendApplicationClient(client_id=client_id)
-    #oauth = OAuth2Session(client=client)
-
-    #token = oauth.fetch_token(token_url='https://accounts.zoho.com/oauth/v2/token?grant_type=authorization_code', client_id=client_id,
-    #client_secret=client_secret)
-    """
 
     def token_saver(token):
         envars['OAUTH_TOKEN'] = token
@@ -43,7 +23,6 @@ def main(args):
         'client_id': client_id,
         'client_secret': client_secret,
     }
-    # After updating the token you will most likely want to save it.
     client = OAuth2Session(client_id, token=token, auto_refresh_url=refresh_url,
         auto_refresh_kwargs=extra, token_updater=token_saver)
 
