90c4aec2d67a00a6ecbfea9e9425242dc55416a9
Cameron Letch <4852986+CamDL@users.noreply.github.com>
2022-02-18 22:46:20 -0500

    Rebuilt requirements.txt

--- server/packages/rugs/rug/__main__.py
+++ server/packages/rugs/rug/__main__.py
@@ -2,14 +2,10 @@ import os
 from requests_oauthlib import OAuth2Session
 
 def main(args):
-    #main({"ID":"3183625000003900011"})
     ID = args.get("ID")
     envars = os.environ
     client_id = envars['CLIENT_ID']
     client_secret = envars['CLIENT_SECRET']
-
-    def token_saver(token):
-        envars['OAUTH_TOKEN'] = token
 
     token = {
         'access_token': envars['ACCESS_TOKEN'],
@@ -22,7 +18,7 @@ def main(args):
         'client_id': client_id,
         'client_secret': client_secret,
     }
-    client = OAuth2Session(client_id, token=token, auto_refresh_url=refresh_url, auto_refresh_kwargs=extra, token_updater=token_saver)
+    client = OAuth2Session(client_id, token=token, auto_refresh_url=refresh_url, auto_refresh_kwargs=extra)
 
     client.refresh_token(refresh_url)
 
