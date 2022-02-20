dc2f45812baf4078358e06834c550d22c75d0de8
Cameron Letch <4852986+CamDL@users.noreply.github.com>
2022-02-19 19:42:22 -0500

    Testing timeout

--- server/packages/rugs/rug/__main__.py
+++ server/packages/rugs/rug/__main__.py
@@ -9,8 +9,8 @@ def store_image(id, field, response):
     s3session = boto3.session.Session()
     s3client = s3session.client('s3', region_name='nyc3',
         endpoint_url='https://nyc3.digitaloceanspaces.com',
-        aws_access_key_id=os.getenv('SPACES_KEY'),
-        aws_secret_access_key=os.getenv('SPACES_SECRET'))
+        aws_access_key_id=os.environ.get('SPACES_KEY'),
+        aws_secret_access_key=os.environ.get('SPACES_SECRET'))
     ext = mimetypes.guess_extension(response.headers['Content-Type'].partition(';')[0].strip())
     key = f'{id}_{field}.{ext}'
     s3client.put_object(Bucket=bucket,
