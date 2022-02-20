d70876ab9c86ca87a41a4afd1be92f1b60ec5206
Cameron Letch <4852986+CamDL@users.noreply.github.com>
2022-02-19 21:02:19 -0500

    Moved getting rug info back into main

--- server/packages/rugs/rug/__main__.py
+++ server/packages/rugs/rug/__main__.py
@@ -28,13 +28,6 @@ def initialize_connection():
     except:
         return None
 
-def get_rug(client, id):
-    try:
-        rug = client.get(f"https://creator.zoho.com/api/v2/troylusk/cleaning-process/report/Rug_Information_Report/{id}").json()
-        return rug
-    except:
-        return None
-
 def get_images(rug, client):
     try:
         base_url = 'https://creator.zoho.com'
@@ -73,8 +66,27 @@ def store_image(id, field, response):
 def main(args):
     #main({"ID":"3183625000003900011"})
     ID = args.get("ID")
-    client = initialize_connection()
-    rug = get_rug(client,ID)
+    client_id = os.environ.get('CLIENT_ID')
+    client_secret = os.environ.get('CLIENT_SECRET')
+
+    token = {
+        'access_token': os.environ.get('ACCESS_TOKEN'),
+        'refresh_token': os.environ.get('REFRESH_TOKEN'),
+        'token_type': 'Bearer',
+        'expires_in': os.environ.get('EXPIRES_IN'),
+    }
+    refresh_url = 'https://accounts.zoho.com/oauth/v2/token'
+    extra = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+    }
+    client = OAuth2Session(client_id, token=token, auto_refresh_url=refresh_url,
+        auto_refresh_kwargs=extra)
+
+    client.refresh_token(refresh_url)
+    
+    rug = client.get(f"https://creator.zoho.com/api/v2/troylusk/cleaning-process/report/Rug_Information_Report/{ID}").json()
+
     if rug:
         return { "body": rug }
     else:
