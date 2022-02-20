83da8d3f8156be4f3d958ee58964e98b8e1f0b92
Cameron Letch <4852986+CamDL@users.noreply.github.com>
2022-02-19 23:58:08 -0500

    Moved environment get

--- server/packages/files/list/__main__.py
+++ server/packages/files/list/__main__.py
@@ -3,11 +3,13 @@ import boto3
 
 def main(args):
     bucket = 'cdl-doserverless'
+    key = os.environ.get('SPACES_KEY')
+    secret = os.environ.get('SPACES_SECRET')
     session = boto3.session.Session()
     client = session.client('s3', region_name='nyc3',
         endpoint_url='https://nyc3.digitaloceanspaces.com',
-        aws_access_key_id=os.environ.get('SPACES_KEY'),
-        aws_secret_access_key=os.environ.get('SPACES_SECRET'))
+        aws_access_key_id=key,
+        aws_secret_access_key=secret)
     files = []
     response = client.list_objects(Bucket=bucket)
     for obj in response['Contents']:
